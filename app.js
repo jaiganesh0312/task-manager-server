abfe97a0e2106fe972e393a514686a509d9d1bfa
jaiganesh0312 <jgtumkar@gmail.com>
2026-02-21 23:30:03 +0530

    fix(helmet): remove unsafe-line for scripts and css

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-            styleSrc: ["'self'", "'unsafe-inline'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'"],
             imgSrc: ["'self'", "data:", "https:"],
-            connectSrc: ["'self'", process.env.CLIENT_URL],
+            connectSrc: ["'self'", process.env.CLIENT_URL,],
             fontSrc: ["'self'", "https:", "data:"],
             objectSrc: ["'none'"],
             mediaSrc: ["'self'"],
